002b446dbd792ac1522bca2a005f0ebcd011aaaf
Ryan Mammina <rmammina@gmail.com>
2019-07-27 00:34:37 -0400

    handle null cases, run more tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var expect = require('chai').expect;
 var Moz = require('../lib/Moz');
 var Signature = require('../lib/signature');
 var MozEndpoint = require('../lib/moz-endpoint');
+var utils = require('../lib/utils');
 
 const credentials = {
   accessId: process.env.ACCESS_ID,
@@ -28,7 +29,7 @@ describe('Moz', () => {
       expect(function() { new Moz({
         secretKey: credentials.secretKey
       })}).to.throw(Error);
-    }); 
+    });
 
     it('has defined properties', () => {
       var moz = new Moz(credentials);
@@ -65,6 +66,30 @@ describe('Moz', () => {
         expect(moz[endpoint]).to.be.an.instanceof(MozEndpoint) 
       }
     });
+
+    it('updates expires to different threshold', () => {
+      var moz = new Moz(credentials);
+      moz.setExpires(500);
+      expect(moz._api.expiresThreshold).to.equal(500);
+    });
+
+    it('updates expires to default threshold with no param', () => {
+      var moz = new Moz(credentials);
+      moz.setExpires(1000);
+      moz.setExpires();
+
+      expect(moz._api.expiresThreshold).to.equal(300);
+    })
+
+    it('explains property from definitions', () => {
+      var moz = new Moz(credentials);
+      expect(moz.explain('apu')).to.equal('Returns phrases found in links to the target URL');
+    })
+
+    it('has no explain for undefined definition', () => {
+      var moz = new Moz(credentials);
+      expect(moz.explain('moz')).to.equal('No explanation');
+    })
   });
 });
 
@@ -88,4 +113,110 @@ describe('Signature', () => {
     expect(moz.signature.getExpires()).to.equal(moz.signature.expires(300))
   });
 
-})
+});
+
+describe('Utils', () => {
+  describe('toTitleCase', () => {
+    it('converts string to titlecase', () => {
+      var string = 'hello world';
+      expect(utils.toTitleCase(string)).to.equal('Hello World');
+    });
+
+    it('only capitalizes first letter of word', () => {
+      var string = 'seoRocksTheWorld';
+
+      expect(utils.toTitleCase(string)).to.equal('Seorockstheworld');
+    });
+  });
+
+  describe('capitalize', () => {
+    it('capitalizes a string', () => {
+      var string = 'titleWithCapitals';
+
+      expect(utils.capitalize(string)).to.equal('TitleWithCapitals');
+    });
+  });
+
+  describe('pascalToCamelCase', () => {
+    it('converts pascal case to camel case', () => {
+      var string = 'ClassName';
+      expect(utils.pascalToCamelCase(string)).to.equal('className');
+    });
+  })
+
+  describe('isValidURL', () => {
+    it('validates an url as correct', () => {
+      var url = 'https://www.example.com';
+      expect(utils.isValidURL(url)).to.equal(true);
+
+      var url = 'https://example.io';
+      expect(utils.isValidURL(url)).to.equal(true);
+
+      var url = 'example.org';
+      expect(utils.isValidURL(url)).to.equal(true);
+    });
+
+    it('validates an url as incorrect', () => {
+      var url = 'https:/www.example.com';
+      expect(utils.isValidURL(url)).to.equal(false);
+
+      var url = 'www,examplecom';
+      expect(utils.isValidURL(url)).to.equal(false);
+
+      var url = 'ftp://www.example.c';
+      expect(utils.isValidURL(url)).to.equal(false);
+    });
+  });
+
+  describe('isArray', ()  => {
+    it('correctly validates array', () => {
+      expect(utils.isArray([1,2,3])).to.equal(true);
+      expect(utils.isArray([])).to.equal(true);
+
+      expect(utils.isArray(new Array(1,2,3))).to.equal(true);
+      expect(utils.isArray(new Array(1))).to.equal(true);
+      expect(utils.isArray(new Array())).to.equal(true);
+    });
+
+    it('correctly validates incorrect array type', () => {
+      expect(utils.isArray("array")).to.equal(false);
+      expect(utils.isArray(1)).to.equal(false);
+      expect(utils.isArray({array: []})).to.equal(false);
+      expect(utils.isArray({})).to.equal(false);
+      expect(utils.isArray(function() {})).to.equal(false);
+      expect(utils.isArray(null)).to.equal(false);
+      expect(utils.isArray(undefined)).to.equal(false);
+      expect(utils.isArray()).to.equal(false);
+    });
+  })
+
+  describe('hasProperty', () => {
+    it('finds property on object', () => {
+      var obj = {
+        prop: 'hello world'
+      };
+
+      expect(utils.hasProperty(obj, 'prop')).to.equal(true);
+
+      var obj = {
+        add: function(a, b) { return a + b }
+      }
+
+      expect(utils.hasProperty(obj, 'add')).to.equal(true);
+    });
+
+    it('does not find property on object', () => {
+      expect(utils.hasProperty({}, 'prop')).to.equal(false);
+      expect(utils.hasProperty('', 'prop')).to.equal(false);
+      expect(utils.hasProperty(null, 'prop')).to.equal(false);
+      expect(utils.hasProperty(undefined, 'prop')).to.equal(false);
+
+      var obj = {};
+      obj.prototype = {
+        'prop': 123
+      }
+
+      expect(utils.hasProperty(obj, 'prop')).to.equal(false)
+    })
+  })
+});
